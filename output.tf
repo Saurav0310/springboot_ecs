4faea9a4682d3f76f8bc42828617ba3c65d43170
Saurav Thakur <91631978+Saurav0310@users.noreply.github.com>
2022-12-16 00:08:27 +0530

    Update output.tf

--- output.tf
+++ output.tf
@@ -1,18 +1,18 @@
 output "alb_dns_name" {
   value = module.ecs.alb_dns_name
 }
-output "task-definition" {
-  value = module.ecs.task_definition
-}
-output "rds_user_password" {
-  value = module.rds.rdsdbpass
-}
-output "rds-username" {
-  value = module.rds.username
-}
-output "rds-endpoint" {
-  value = module.rds.endpoint
-}
-output "db_name" {
-  value = module.rds.db_name
-}
+# output "task-definition" {
+#   value = module.ecs.task_definition
+# }
+# output "rds_user_password" {
+#   value = module.rds.rdsdbpass
+# }
+# output "rds-username" {
+#   value = module.rds.username
+# }
+# output "rds-endpoint" {
+#   value = module.rds.endpoint
+# }
+# output "db_name" {
+#   value = module.rds.db_name
+# }
