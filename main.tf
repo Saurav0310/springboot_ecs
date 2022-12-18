820b3e344998d09436847905d568916532262103
Saurav Thakur <91631978+Saurav0310@users.noreply.github.com>
2022-12-18 11:01:31 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -20,21 +20,21 @@ module "ecs" {
   source     = "./modules/ecs"
   asg_arn    = module.asg.asg_arn
   name       = var.name
-  vpc_id     = module.network.vpcid   #target_group_vpcid
-  public_sg  = [module.sgALB.sgid]    #ALBsecuritygroup&public_subnet
+  vpc_id     = module.network.vpcid   
+  public_sg  = [module.sgALB.sgid]    
   public_sub = [module.network.public_subnet_ids1, module.network.public_subnet_ids2]
-  path = "/swagger-ui.html" #target_group_health_check_path
+  path = "/swagger-ui.html" 
   imageURI         = "${var.imageURI}"
-  container_cpu    = 512
-  container_memory = 736
-  containerPort    = 8090
-  hostPort         = 8090 #0
-  autoscaling_cpu  = true
-  autoscaling_target_cpu = 70
   ssm_variables    = { "DB_ENDPOINT" : module.rds.ssm_parameter_rds_endpoint, 
                        "DB_NAME" : module.rds.ssm_parameter_rds_dbname, 
                        "DB_USER" : module.rds.ssm_parameter_rds_user, 
                        "DB_PASS" : module.rds.ssm_parameter_rds_password }
+  container_cpu    = 1024
+  container_memory = 1024
+  containerPort    = 8090
+  hostPort         = 8090 #0
+  autoscaling_cpu  = true
+  autoscaling_target_cpu = 70
 }
 
                          
@@ -43,8 +43,8 @@ module "sgASG" {
   name      = "ecs"
   sg_cidr   = [module.network.cidr_block]
   sg_vpc_id = module.network.vpcid
-  from_port = 8090      #49153  #8090for dynamic port 
-  to_port   = 8091     #65535  #8091
+  from_port = 8090   
+  to_port   = 8091    
 }
     
 
