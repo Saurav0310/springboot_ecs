843cd8524f04ea84c98f4f53890b49d05737b3dd
Saurav Thakur <91631978+Saurav0310@users.noreply.github.com>
2022-12-19 12:06:45 +0530

    Update ecs.tf

--- modules/ecs/ecs.tf
+++ modules/ecs/ecs.tf
@@ -6,12 +6,6 @@ resource "aws_ecs_cluster_capacity_providers" "ecs_capacity_providers" {
   cluster_name = aws_ecs_cluster.ecs_cluster.name
   capacity_providers = [aws_ecs_capacity_provider.ecs_capacity_provider.name]
 
-  #default_capacity_provider_strategy {
-  #  base              = 1
-  #  weight            = 100
-  #  capacity_provider = aws_ecs_capacity_provider.ecs_capacity_provider.name
-  #}
-}
 
 resource "aws_ecs_capacity_provider" "ecs_capacity_provider" {
   name = "provider-${var.name}"
