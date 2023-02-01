0a0cece295f3413284688616bf184f8fe6e859db
Saurav Thakur <91631978+Saurav0310@users.noreply.github.com>
2023-02-02 00:15:21 +0530

    Update checkout.groovy

--- vars/checkout.groovy
+++ vars/checkout.groovy
@@ -1,7 +1,6 @@
 def call(Map stageParams) {
 
     checkout([
-        $class: 'GitSCM',
         branches: [[ name:"main"  stageParams.branch ]],
         userRemoteConfigs: [[ url:"https://github.com/Saurav0310/springboot_ecs.git" stageParams.url ]]
     ])
