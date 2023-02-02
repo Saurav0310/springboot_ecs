2afab97d480203d8c8a7aec91d40234615e78976
Saurav Thakur <91631978+Saurav0310@users.noreply.github.com>
2023-02-02 16:26:46 +0530

    Update npm.groovy

--- vars/npm.groovy
+++ vars/npm.groovy
@@ -1,3 +1,41 @@
-def call(String name = 'User') {
- echo "Welcome, ${name}."
+
+#!/usr/bin/env groovy
+
+/**
+ * Send notifications based on build status string
+ */
+def call(String buildStatus = 'STARTED') {
+  // build status of null means successful
+  buildStatus = buildStatus ?: 'SUCCESS'
+
+  // Default values
+  def colorName = 'RED'
+  def colorCode = '#BD3855'
+  def subject = "${buildStatus}: Job '${env.JOB_NAME} [${env.BUILD_NUMBER}]'"
+  def summary = "${subject} (${env.BUILD_URL})"
+  def details = """<p>${buildStatus}: Job '${env.JOB_NAME} [${env.BUILD_NUMBER}]':</p>
+    <p>Check console output at &QUOT;<a href='${env.BUILD_URL}'>${env.JOB_NAME} [${env.BUILD_NUMBER}]</a>&QUOT;</p>"""
+
+  // Override default values based on build status
+  if (buildStatus == 'STARTED') {
+    color = 'YELLOW'
+    colorCode = '#DAAD4F'
+  } else if (buildStatus == 'SUCCESS') {
+    color = 'GREEN'
+    colorCode = '#5CB589'
+  } else {
+    color = 'RED'
+    colorCode = '#BD3855'
+  }
+
+  // Send notifications
+//   slackSend (color: colorCode, message: summary)
+
+  emailext (
+      to: 'thakursaurav339@gmail.com',
+      subject: subject,
+      body: details,
+      recipientProviders: [[$class: 'DevelopersRecipientProvider']]
+    )
+
 }
