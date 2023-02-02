7d03c0ff1bbe79bd6a2f64581b2f6530df4bcf7a
Saurav Thakur <91631978+Saurav0310@users.noreply.github.com>
2023-02-02 13:21:57 +0530

    Update npm.groovy

--- vars/npm.groovy
+++ vars/npm.groovy
@@ -1,3 +1,39 @@
-def call(String name = 'User') {
- echo "Welcome, ${name}."
+def call(var1,var2,folder){
+pipeline {
+    agent any
+    stages {
+        stage('Git checkout') {
+           steps{
+                git branch: 'qa_release', credentialsId: '', url: 'git@github.com:Saurav0310/springboot_ecs.git'
+            }
+        }
+        stage('build image') {
+           steps{
+               script {
+                   sh script:'''
+                   #!/bin/bash
+                   set +x
+                   cd ${folder}/
+                   ls
+                   docker build -t 991037556739.dkr.ecr.us-east-1.amazonaws.com/nw-social-${var1}:${var2}-${BUILD_NUMBER} Dockerfile .
+                   docker tag 991037556739.dkr.ecr.us-east-1.amazonaws.com/nw-social-${var1}:${var2}-${BUILD_NUMBER} 743930152640.dkr.ecr.us-east-1.amazonaws.com/nw-social-${var1}:${var2}-latest
+                   aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 991037556739.dkr.ecr.us-east-1.amazonaws.com
+                   docker push 991037556739.dkr.ecr.us-east-1.amazonaws.com/nw-social-${var1}:${var2}-${BUILD_NUMBER}
+                   docker push 991037556739.dkr.ecr.us-east-1.amazonaws.com/nw-social-${var1}:${var2}-latest
+                   docker rmi -f 991037556739.dkr.ecr.us-east-1.amazonaws.com/nw-social-${var1}:${var2}-${BUILD_NUMBER}
+                   docker rmi -f 991037556739.dkr.ecr.us-east-1.amazonaws.com/nw-social-${var1}:${var2}-latest
+                   '''
+               }
+            }
+        }
+
+    //     stage('kubernetes deploy') {
+    //        steps{
+    //             sh 'kubectl config set-context --namespace=default --current'
+    //             sh 'kubectl set image deployment/nw-social-${var1}-${var2} nw-social-${var1}-${var2}=743930152640.dkr.ecr.us-east-1.amazonaws.com/nw-social-${var1}:${var2}-${BUILD_NUMBER}'
+    //             sh 'kubectl rollout status deployment.v1.apps/nw-social-${var1}-${var2}'
+    //         }
+    //     }
+    }
+ }
 }
