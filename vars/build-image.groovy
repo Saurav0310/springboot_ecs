0ee4b93c70e74e56d69cd9634446e8b51eac1ca7
Saurav Thakur <91631978+Saurav0310@users.noreply.github.com>
2023-02-03 23:14:13 +0530

    Update build-image.groovy

--- vars/build-image.groovy
+++ vars/build-image.groovy
@@ -2,7 +2,7 @@ def call(){
  echo "Building java project"
   sh '''
   set +x
-  cd ${JOB_DIR}
+  cd \$JOB_DIR
   ls
   docker build -t ${DOCKER_REGISTRY}/nw-social-${DOCKER_SERVICE}:${ENV}-${env.BUILD_NUMBER} Dockerfile .
   docker tag ${DOCKER_REGISTRY}/nw-social-${DOCKER_SERVICE}:${ENV}-${env.BUILD_NUMBER} ${DOCKER_REGISTRY}/nw-social-${DOCKER_SERVICE}:${ENV}-latest  
