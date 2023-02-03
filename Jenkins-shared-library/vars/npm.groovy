32563e5b358df649ee4ac84066224284a5f73520
Saurav Thakur <91631978+Saurav0310@users.noreply.github.com>
2023-02-03 10:52:57 +0530

    Update npm.groovy

--- Jenkins-shared-library/vars/npm.groovy
+++ Jenkins-shared-library/vars/npm.groovy
@@ -1,37 +1,31 @@
-def call(){
+#!/usr/bin/env groovy
 
-pipeline {
-    stages {
-        
-        stage('npm install') { 
-            steps {
-                sh 'npm install' 
-            }
-        }
+/**
+ * Send notifications based on build status string
+ */
+def call(String buildStatus = 'STARTED') {
+  // build status of null means successful
+  buildStatus = buildStatus ?: 'SUCCESS'
 
-        stage('npm run test') { 
-            steps {
-                sh 'npm run test' 
-            }
-        }
+  // Default values
+  def colorName = 'RED'
+  def colorCode = '#BD3855'
+  def subject = "${buildStatus}: Job '${JOB_NAME} [${BUILD_NUMBER}]'"
+  def summary = "${subject} (${BUILD_URL})"
+  def details = """<p>${buildStatus}: Job '${JOB_NAME} [${BUILD_NUMBER}]':</p>
+    <p>Check console output at &QUOT;<a href='${BUILD_URL}'>${JOB_NAME} [${BUILD_NUMBER}]</a>&QUOT;</p>"""
 
-        stage('npm Publish') {
-            steps {
-                sh 'npm publish'
-            }
-        }
-
-        stage('Clean workspace'){
-            steps{
-                echo "Cleaning up ${WORKSPACE}"
-                // clean up our workspace
-                deleteDir()
-                // clean up tmp directory
-                dir("${workspace}@tmp") {
-                deleteDir()          
-                }
-            }
-        }
-    }
-}
+  // Override default values based on build status
+  if ( buildResult == "SUCCESS" ) {
+    slackSend color: "good", message: "Job: ${JOB_NAME} with buildnumber ${BUILD_NUMBER} was successful"
+  }
+  else if( buildResult == "FAILURE" ) { 
+    slackSend color: "danger", message: "Job: ${JOB_NAME} with buildnumber ${BUILD_NUMBER} was failed"
+  }
+  else if( buildResult == "UNSTABLE" ) { 
+    slackSend color: "warning", message: "Job: ${JOB_NAME} with buildnumber ${BUILD_NUMBER} was unstable"
+  }
+  else {
+    slackSend color: "danger", message: "Job: ${JOB_NAME} with buildnumber ${BUILD_NUMBER} its resulat was unclear"	
+  }
 }
