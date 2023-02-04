7432343d6767fe2e3f7408a60de4d33a1ecb247c
Saurav Thakur <91631978+Saurav0310@users.noreply.github.com>
2023-02-04 20:03:55 +0530

    Update push_image.groovy

--- vars/push_image.groovy
+++ vars/push_image.groovy
@@ -1,6 +1,6 @@
 def call() {
   echo "Pushing docker image to ECR"
   sh "aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 991037556739.dkr.ecr.us-east-1.amazonaws.com"
-  sh "docker tag springboot:\${env.BUILD_NUMBER} 991037556739.dkr.ecr.us-east-1.amazonaws.com/springboot:\${env.BUILD_NUMBER}"
-  sh "docker push 991037556739.dkr.ecr.us-east-1.amazonaws.com/springboot:\${env.BUILD_NUMBER}"
+  sh "docker tag springboot:${env.BUILD_NUMBER} 991037556739.dkr.ecr.us-east-1.amazonaws.com/springboot:${env.BUILD_NUMBER}"
+  sh "docker push 991037556739.dkr.ecr.us-east-1.amazonaws.com/springboot:${env.BUILD_NUMBER}"
 }
