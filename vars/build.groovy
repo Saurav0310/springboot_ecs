db061f146f5af42815ac595438c4a55b7636e5f1
Saurav Thakur <91631978+Saurav0310@users.noreply.github.com>
2023-02-04 11:43:47 +0530

    Update build.groovy

--- vars/build.groovy
+++ vars/build.groovy
@@ -1,6 +1,7 @@
 def call(Map config = [:]) {
   echo "Building docker image"
   sh """
+  cd post
   ls
   docker build -t new-${env.BUILD_NUMBER} Dockerfile .
   docker tag new-${env.BUILD_NUMBER}:latest  
