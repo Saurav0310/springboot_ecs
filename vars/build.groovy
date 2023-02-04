c8b81fdb349944251b5f76ee588c799800a40f21
Saurav Thakur <91631978+Saurav0310@users.noreply.github.com>
2023-02-04 12:18:33 +0530

    Update build.groovy

--- vars/build.groovy
+++ vars/build.groovy
@@ -9,7 +9,6 @@
 // }
 def call() {
   echo "Building docker image"
-  sh "cd post/"
   sh "ls"
-    sh "docker build -t \${APP_NAME}:\${BUILD_NUMBER} ."
+  sh "docker build -t \${APP_NAME}:\${BUILD_NUMBER} ."
 }
