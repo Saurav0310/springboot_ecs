f822b44dcb212f64f4e9801938327887874751f4
Saurav Thakur <91631978+Saurav0310@users.noreply.github.com>
2023-02-04 21:03:26 +0530

    Update checkout.groovy

--- vars/checkout.groovy
+++ vars/checkout.groovy
@@ -2,6 +2,6 @@ def call() {
     checkout([
 	                $class: 'GitSCM', 
 	                branches: [[name: '*/main']],  
-	                userRemoteConfigs: [[credentialsId: 'fdb7af1b-09f2-40fd-a3dc-39c69fd02ee4', url: 'https://github.com/Saurav0310/springboot_ecs.git']]
+	                userRemoteConfigs: [[credentialsId: 'fdb7af1b-09f2-40fd-a3dc-39c69fd02ee4', url: REPO_URL]]
 	                ])
 }
