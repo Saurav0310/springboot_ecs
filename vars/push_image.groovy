50933a371bef03bf7caaecbc383d63d3454324ac
Saurav Thakur <91631978+Saurav0310@users.noreply.github.com>
2023-02-04 20:02:04 +0530

    Update push_image.groovy

--- vars/push_image.groovy
+++ vars/push_image.groovy
@@ -1,10 +1,6 @@
 def call() {
   echo "Pushing docker image to ECR"
-  sh "aws ecr get-login-password --region \${REGION} | docker login --usernadocker build -t \${DOCKER_REGISTRY}/nw-social-\${DOCKER_SERVICE}:\${ENV}-\${env.BUILD_NUMBER} Dockerfile ."
-  sh "docker tag \${DOCKER_REGISTRY}/nw-social-\${DOCKER_SERVICE}:\${ENV}-\${env.BUILD_NUMBER} \${DOCKER_REGISTRY}/nw-social-\${DOCKER_SERVICE}:\${ENV}-latestme AWS --password-stdin ${DOCKER_REGISTRY}"
-  sh "docker push \${DOCKER_REGISTRY}/nw-social-\${DOCKER_SERVICE}:\${ENV}-\${env.BUILD_NUMBER}"
-  sh "docker push \${DOCKER_REGISTRY}/nw-social-\${DOCKER_SERVICE}:\${ENV}-latest"
-  sh "docker rmi -f \${DOCKER_REGISTRY}/nw-social-\${DOCKER_SERVICE}:\${ENV}-\${env.BUILD_NUMBER}"
-  sh "docker rmi -f \${DOCKER_REGISTRY}/nw-social-\${DOCKER_SERVICE}:\${ENV}-latest"
-  sh "rm -r /var/lib/jenkins/.docker/config.json"
+  sh "aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 991037556739.dkr.ecr.us-east-1.amazonaws.com"
+  sh "docker tag springboot:\${env.BUILD_NUMBER} 991037556739.dkr.ecr.us-east-1.amazonaws.com/springboot:\${env.BUILD_NUMBER}"
+  sh "docker push 991037556739.dkr.ecr.us-east-1.amazonaws.com/springboot:\${env.BUILD_NUMBER}"
 }
