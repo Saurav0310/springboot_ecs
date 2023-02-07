57412a8590bcfa013a682ba264b0f3b066d24761
Saurav Thakur <91631978+Saurav0310@users.noreply.github.com>
2023-02-07 15:24:26 +0530

    Update build.groovy

--- vars/build.groovy
+++ vars/build.groovy
@@ -5,7 +5,7 @@ def call() {
                    set +x
                    pwd
                    ls
-                   cd NW-Social-Client/
+                   cd post/
                    ls
                    docker build -t ${APP_NAME}:${BUILD_NUMBER} .
                    '''
